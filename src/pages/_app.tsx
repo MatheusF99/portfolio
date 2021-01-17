5729801965e6a59e099d73b0053a3de79958df82
matheusF99 <mc79846@gmail.com>
2021-01-16 23:43:27 -0300

    configurando theme

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import { AppProps } from 'next/app'
+
+import GlobalStyles from '../styles/global'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default MyApp
