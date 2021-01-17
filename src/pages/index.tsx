8d802683ee8039e28671dc4f4487aa1d077077dc
matheusF99 <mc79846@gmail.com>
2021-01-17 00:39:14 -0300

    estrutura terminada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,19 @@ import React from 'react'
 import Head from 'next/head'
 
 import LoveLogo from '../assets/undraw_Appreciation_re_p6rl.svg'
+import { Container } from '../styles/pages/Home'
+
 const Home: React.FC = () => {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <LoveLogo />
-        <h1>hello world</h1>
-      </main>
-    </div>
+      <LoveLogo />
+      <h1>React Struture</h1>
+      <p>A Typescript + NextJS Struture by with Love</p>
+    </Container>
   )
 }
 
