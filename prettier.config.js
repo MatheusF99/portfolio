24a3b382098ecc5b63c3c9a3eca3d5b5d4f7f235
matheusF99 <mc79846@gmail.com>
2021-01-16 23:31:00 -0300

    configurando o styled component

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-	semi: false,
-	singleQuote: true,
-	arrowParens: "avoid",
-	trailingComma: "none",
-	endOfline: "auto",
-};
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    trailingComma: 'none',
+    endOfline: 'auto'
+}
