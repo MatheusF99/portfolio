b3396d969abdf08251e2ccb12d5dc91fa80d3676
MatheusF99 <mc79846@gmail.com>
2021-02-28 20:49:30 -0300

    background inserido

--- src/components/Landing/landing.tsx
+++ src/components/Landing/landing.tsx
@@ -3,19 +3,40 @@ import React from 'react'
 import { Container } from './styleLanding'
 
 import LogoIcon from '../../assets/logo.svg'
+import BackgroundImage from '../../assets/Background-big.svg'
 
 const Landing: React.FC = () => {
   return (
     <Container>
-      <header>
-        <LogoIcon />
+      <BackgroundImage />
 
-        <div className="toggle-Button">
-          <div className="one"></div>
-          <div className="two"></div>
-          <div className="three"></div>
-        </div>
-      </header>
+      <div className="landing">
+        <header>
+          <LogoIcon />
+
+          <div className="toggle-Button">
+            <div className="one"></div>
+            <div className="two"></div>
+            <div className="three"></div>
+          </div>
+        </header>
+
+        <section>
+          <div className="div-1">
+            <div className="apresentation-div">
+              <span className="hello">Hello</span>
+              <strong>I'm Mathews</strong>
+              <span className="profile" >Web Developer</span>
+            </div>
+
+            <button type="button"> About me </button>
+
+          </div>
+          <div className="div-2">
+            <h1>hello world</h1>
+          </div>
+        </section>
+      </div>
     </Container>
   )
 }
--- src/components/Landing/styleLanding.ts
+++ src/components/Landing/styleLanding.ts
@@ -1,24 +1,40 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
 
   display: flex;
   justify-content: center;
   align-items: center;
 
-  flex-direction: column;
+  position: relative;
 
-  background: ${props => props.theme.colors.background};
   color: ${props => props.theme.colors.text};
 
-  header{
+  svg{
+    height: 100vh;
+    width: 100vw;
+  }
 
-    background: ${props => props.theme.colors.blueDark};
+  .landing{
+
+    position: absolute;
+
+    width: 90vw;
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    flex-direction: column;
+
+    max-width: 1440px;
+
+    header{
+
+    /* background: ${props => props.theme.colors.blueDark}; */
 
     width: 100%;
-    max-width: 1676px;
     height: 80px;
 
     display: flex;
@@ -45,18 +61,82 @@ export const Container = styled.div`
 
       .one, .three{
         background: #000;
-        height: 5px;
-        width: 100%;
+        height: 4px;
+        width: 60%;
         margin: .1rem 0;
       }
 
       .two{
         background: #000;
-        height: 5px;
-        width: 60%;
-        margin: .5rem 0 .5rem 1.6rem;
+        height: 4px;
+        width: 50%;
+        margin: .1rem 0 .05rem 0.4rem;
 
       }
     }
+  }
+
+  section{
+
+    width: 100%;
+    height: 80%;
+
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+
+    .div-1{
+      height: 90%;
+      display: flex;
+
+      flex-direction: column;
+
+      padding: 0 5rem;
+
+      /* background: ${props => props.theme.colors.red}; */
+
+      .apresentation-div{
+
+        flex: 1;
+
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        flex-direction: column;
+
+        color: #000;
+
+        .hello{
+          font-size: 4rem;
+        }
+
+        strong{
+          font-size: 8rem;
+          font-weight: 600;
+        }
+        .profile{
+          font-size: 5rem;
+        }
+
+      }
+      button{
+        width: 245px;
+        height: 80px;
+
+        border: 0;
+
+        background: ${props => props.theme.colors.primary};
+        color: ${props => props.theme.colors.white};
+
+        border-radius: 25px;
+
+        font-size: 2.5rem;
+
+      }
+    }
+    .div-2{
+      background: ${props => props.theme.colors.grayLine};
+    }
+  }
   }
 `
